fad4185eddf8bd0d624eac1fc64865c2c791bc03
Brian Durham <47435950+thebriandurham@users.noreply.github.com>
2022-07-29 22:48:35 -0400

    add console tutorial

---
+++ console_tutorial/tutorial_5/target/debug/deps/tutorial_5-0debe456595b2ee2.d
@@ -0,0 +1,5 @@
+/home/kali/rust/console_tutorial/tutorial_5/target/debug/deps/tutorial_5-0debe456595b2ee2: src/main.rs
+
+/home/kali/rust/console_tutorial/tutorial_5/target/debug/deps/tutorial_5-0debe456595b2ee2.d: src/main.rs
+
+src/main.rs:
---
+++ console_tutorial/tutorial_5/target/debug/tutorial_5.d
@@ -0,0 +1,1 @@
+/home/kali/rust/console_tutorial/tutorial_5/target/debug/tutorial_5: /home/kali/rust/console_tutorial/tutorial_5/src/main.rs
