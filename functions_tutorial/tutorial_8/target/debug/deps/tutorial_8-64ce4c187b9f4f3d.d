d47901040f3829c831296985f6551e215a56912c
Brian Durham <47435950+thebriandurham@users.noreply.github.com>
2022-07-29 22:50:46 -0400

    add functions tutorial

---
+++ functions_tutorial/tutorial_8/target/debug/deps/tutorial_8-64ce4c187b9f4f3d.d
@@ -0,0 +1,5 @@
+/home/kali/rust/functions_tutorial/tutorial_8/target/debug/deps/tutorial_8-64ce4c187b9f4f3d: src/main.rs
+
+/home/kali/rust/functions_tutorial/tutorial_8/target/debug/deps/tutorial_8-64ce4c187b9f4f3d.d: src/main.rs
+
+src/main.rs:
---
+++ functions_tutorial/tutorial_8/target/debug/tutorial_8.d
@@ -0,0 +1,1 @@
+/home/kali/rust/functions_tutorial/tutorial_8/target/debug/tutorial_8: /home/kali/rust/functions_tutorial/tutorial_8/src/main.rs
