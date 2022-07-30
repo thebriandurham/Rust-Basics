552b9fb5e082ad9b86d22583bed83d1ec73e329c
Brian Durham <47435950+thebriandurham@users.noreply.github.com>
2022-07-29 22:47:24 -0400

    add arithmetic tutorial

---
+++ arithmetic_and_casting_tutorial/tutorial_6/target/debug/deps/tutorial_6-1de3edcb77f243b7.d
@@ -0,0 +1,5 @@
+/home/kali/rust/arithmetic_and_casting_tutorial/tutorial_6/target/debug/deps/tutorial_6-1de3edcb77f243b7: src/main.rs
+
+/home/kali/rust/arithmetic_and_casting_tutorial/tutorial_6/target/debug/deps/tutorial_6-1de3edcb77f243b7.d: src/main.rs
+
+src/main.rs:
---
+++ arithmetic_and_casting_tutorial/tutorial_6/target/debug/tutorial_6.d
@@ -0,0 +1,1 @@
+/home/kali/rust/arithmetic_and_casting_tutorial/tutorial_6/target/debug/tutorial_6: /home/kali/rust/arithmetic_and_casting_tutorial/tutorial_6/src/main.rs
